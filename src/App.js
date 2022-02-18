5d0c885315ad3087f931fe16748f0123818de621
Vinh Pham <phamv1@unlv.nevada.edu>
2022-02-17 23:33:11 -0800

    complete basic routing

--- src/App.js
+++ src/App.js
@@ -1,41 +1,53 @@
-import './App.css';
 import { useEffect, useState } from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Gallery from './components/Gallery'
 import SearchBar from './components/SearchBar'
+import AlbumView from './components/AlbumView'
+import ArtistView from './components/ArtistView'
 
 function App() {
-  let [searchTerm, setSearchTerm] = useState('')
-  let [data, setData] = useState([])
-  let [message, setMessage] = useState('Search for Music!')
+let [searchTerm, setSearchTerm] = useState('')
+let [data, setData] = useState([])
+let [message, setMessage] = useState('Search for Music!')
 
-  useEffect(() => {
+
+useEffect(() => {
+    const API_URL = `https://itunes.apple.com/search?term=`
     if (searchTerm) {
-      document.title=`${searchTerm} Music`
-      const fetchData = async () => {
-        const response = await fetch(`https://itunes.apple.com/search?term=${searchTerm}`)
+    document.title=`${searchTerm} Music`
+    const fetchData = async () => {
+        const response = await fetch(API_URL + searchTerm)
         const resData = await response.json()
         if(resData.results.length > 0) {
-          setData(resData.results)
+        setData(resData.results)
         } else {
-          setMessage('Not Found')
+        setMessage('Not Found')
         }
-      }
-      fetchData()
-  }
-  }, [searchTerm])
+    }
+    fetchData()
+}
+}, [searchTerm])
 
-  const handleSearch = (e, term) => {
+const handleSearch = (e, term) => {
     e.preventDefault()
     setSearchTerm(term)
-  }
+}
 
-  return (
-    <div className="App">
-      <SearchBar handleSearch={handleSearch} />
-      {message}
-      <Gallery data={data} />
-    </div>
-  );
+return (
+  <div className="App">
+    {message}
+    
+    <BrowserRouter>
+      <Routes>
+          <Route exact path="/" element= {
+            <><SearchBar handleSearch={handleSearch} /><Gallery data={data} /></>
+          } />
+          <Route path="/album/:id" element= { <AlbumView /> } />
+          <Route path="/artist/:id" element={ <ArtistView /> } />
+      </Routes>
+    </BrowserRouter>
+  </div>
+)
 }
 
 export default App;
--- src/components/GalleryItem.js
+++ src/components/GalleryItem.js
@@ -1,9 +1,11 @@
 import { useState } from 'react'
+import { Link } from 'react-router-dom'
 
 function GalleryItem(props){
     let [view, setView] = useState(false)
 
     const simpleView = () => {
+        console.log(props.item.trackName)
         return (
             <div style={{
                 'width': '25vw',
@@ -18,26 +20,25 @@ function GalleryItem(props){
         )
     }
 
-    const detailView = () => {
-        return (
-            <div style={{
-                'width': '80vw',
-                'height': '20vh',
-                'border': '1px solid black',
-                'margin' : '2px',
-                'position': 'relative',
-                'backgroundImage': `url(${props.item.artworkUrl100})`,
-                'backgroundRepeat': 'no-repeat',
-                'backgroundSize': 'cover',
-                'color': 'white'
-            }}>
-                <h2>{props.item.trackName}</h2>
-                <h3>{props.item.collectionName}</h3>
-                <h4>{props.item.primaryGenreName}</h4>
-                <h4>{props.item.releaseDate}</h4>
-            </div>
-        )
-    }
+
+const detailView = () => {
+    console.log(props)
+    return (
+        <div>
+            <h2>"{props.item.trackName}"</h2>
+            <h3>
+                <Link to={`/artist/${props.item.artistId}`}>
+                    {props.item.artistName}
+                </Link>
+            </h3>
+            <h3>
+                <Link to={`/album/${props.item.collectionId}`}>
+                    {props.item.collectionName}
+                </Link>
+            </h3>
+        </div>
+    )
+}
 
     return (
         <div onClick={() => setView(!view)}
